367db62fbe614ca1ae44e31ebac9fc75d25f7a33
Masanori Onoue <ugaya40@hotmail.com>
2022-08-03 14:24:49 +0900

    Revert "add comment to AsyncZipIterable."
    This reverts commit 7f9218ebd000e1b0c2fcbd63617defa5a3801f24.

--- packages/leseq/src/generators/zip.ts
+++ packages/leseq/src/generators/zip.ts
@@ -12,21 +12,6 @@ export function zip<T1, T2, T3, T4>(source1: Iterable<T1>, source2: Iterable<T2>
 export function zip<T1, T2, T3>(source1: Iterable<T1>, source2: Iterable<T2>, source3: Iterable<T3>): Seq<[T1, T2, T3]>;
 export function zip<T1, T2>(source1: Iterable<T1>, source2: Iterable<T2>): Seq<[T1, T2]>;
 export function zip<T>(...sources: Iterable<T>[]): Seq<T[]>;
-/**
- * Generates a sequence in which the specified value is repeated a specified number of times.
- *
- * ```typescript
- * const result = repeat(5,3).toArray();
- * //result: [5,5,5]
- * ```
- *
- * @param target Repeating Element.
- * @param count Number of pieces to be generated.
- * @returns Sequence.
- * @typeParam T Source element type.
- * @category Generators
- *
- */
 export function zip(...sources: Iterable<unknown>[]): Seq<unknown> {
   return new Seq(new ZipIterable(sources));
 }
