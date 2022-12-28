8a00449a3340b463f805aa82f8b66d22ad8336ea
ugaya40(Masanori Onoue) <ugaya40@hotmail.com>
2022-12-28 09:07:47 +0900

    filter with User Defined Type Guard.

--- packages/leseq/src/operators/async/filterAsync.ts
+++ packages/leseq/src/operators/async/filterAsync.ts
@@ -1,5 +1,8 @@
 import { AsyncGen, AsyncOperator, AsyncSeq } from '../../AsyncSeq';
 
+export function filterAsync<T, TResult extends T>(predicate: (arg: T, index: number) => arg is TResult): AsyncOperator<T, TResult>;
+export function filterAsync<T>(predicate: (arg: T, index: number) => Promise<boolean> | boolean): AsyncOperator<T>;
+
 /**
  * Returns a sequence that has been filtered by the specified condition.
  *
@@ -9,15 +12,25 @@ import { AsyncGen, AsyncOperator, AsyncSeq } from '../../AsyncSeq';
  * ).toArrayAsync();
  * //result: [2,4]
  * ```
+ * 
+ * **with User Defined Type Guard**
+ * ```typescript
+ * const result = await fromAsAsync([1,'a',2,'b'])
+ *   .pipe(
+ *     filterAsync<number | string, string>((i): i is string => typeof i === 'string')
+ *   )
+ *   .toMutableArrayAsync();
+ * //result: ['a','b']
+ * //result type is string[]
+ * ```
  *
  * @param predicate Conditional functions for filtering.
  * @returns Operator function.
  * @typeParam T Source element type.
  * @category Async Operators
  */
-
-export const filterAsync = <T>(predicate: (arg: T, index: number) => Promise<boolean>): AsyncOperator<T> =>
-  async function* filterAsync(source: AsyncSeq<T>): AsyncGen<T> {
+export function filterAsync<T>(predicate: (arg: T, index: number) => Promise<boolean> | boolean): AsyncOperator<T> {
+  return async function* filterAsync(source: AsyncSeq<T>): AsyncGen<T> {
     let count = 0;
     for await (const i of source) {
       if (await predicate(i, count)) {
@@ -25,4 +38,5 @@ export const filterAsync = <T>(predicate: (arg: T, index: number) => Promise<boo
       }
       count++;
     }
-  };
+  }
+}
--- packages/leseq/src/operators/filter.ts
+++ packages/leseq/src/operators/filter.ts
@@ -1,5 +1,8 @@
 import { Gen, Operator, Seq } from '../Seq';
 
+export function filter<T, TResult extends T>(predicate: (arg: T, index: number) => arg is TResult): Operator<T,TResult>;
+export function filter<T>(predicate: (arg: T, index: number) => boolean): Operator<T>;
+
 /**
  * Returns a sequence that has been filtered by the specified condition.
  *
@@ -7,14 +10,25 @@ import { Gen, Operator, Seq } from '../Seq';
  * const result = from([1, 2, 3, 4, 5]).pipe(filter(i => i % 2 == 0)).toArray();
  * //result: [2,4]
  * ```
- *
+ * 
+ * **with User Defined Type Guard**
+ * ```typescript
+ * const result = from([1,'a',2,'b'])
+ *   .pipe(
+ *     filter<number | string, string>((i): i is string => typeof i === 'string')
+ *   )
+ *   .toMutableArray();
+ * //result: ['a','b']
+ * //result type is string[]
+ * ```
+ * 
  * @param predicate Conditional functions for filtering.
  * @returns Operator function.
  * @typeParam T Source element type.
  * @category Operators
  */
-export const filter = <T>(predicate: (arg: T, index: number) => boolean): Operator<T> =>
-  function* filter(source: Seq<T>): Gen<T> {
+export function filter<T>(predicate: (arg: T, index: number) => boolean): Operator<T> {
+  return function* filter(source: Seq<T>): Gen<T> {
     let count = 0;
     for (const i of source) {
       if (predicate(i, count)) {
@@ -22,4 +36,5 @@ export const filter = <T>(predicate: (arg: T, index: number) => boolean): Operat
       }
       count++;
     }
-  };
+  }
+}
