1c7bf2f8b267f216612c9c59dd32fcff677f1d4c
XantreGodlike <valerik007.vs@gmail.com>
2023-08-14 23:57:50 +0300

    Added benchmarks

--- packages/leseq/tests/async-common.test.ts
+++ packages/leseq/tests/async-common.test.ts
@@ -1,33 +1,38 @@
-import { async, filterAsync, findAsync, from, fromAsAsync, mapAsync, reverse, tapAsync } from "../src";
-import { abortableSleep, performanceAsync } from "./testUtil";
+import { async, filterAsync, findAsync, from, fromAsAsync, mapAsync, reverse, tapAsync } from '../src';
+import { abortableSleep, performanceAsync } from './testUtil';
 
 test('async operator: simple', async () => {
-
-  const [output,time] = await performanceAsync(async () =>
-
-    await from([1, 2, 3, 4, 5]).pipe(reverse()).to(async()).pipe(
-      tapAsync(async () => await abortableSleep(20)),
-      mapAsync(async i => {
-        await abortableSleep(20);
-        return i * i;
-      })
-    ).toArrayAsync()
-
+  const [output, time] = await performanceAsync(
+    async () =>
+      await from([1, 2, 3, 4, 5])
+        .pipe(reverse())
+        .to(async())
+        .pipe(
+          tapAsync(async () => await abortableSleep(20)),
+          mapAsync(async i => {
+            await abortableSleep(20);
+            return i * i;
+          })
+        )
+        .toArrayAsync()
   );
 
-  expect(output).toEqual([25,16,9,4,1]);
-  expect(time > 200 && time < 300).toBe(true);
+  expect(output).toEqual([25, 16, 9, 4, 1]);
+  expect(time > 200 && time < 400).toBe(true);
 });
 
 test('async operator: simple 2', async () => {
-  const [output,time] = await performanceAsync(async () =>
-    await fromAsAsync([1,2,3,4,5]).pipe(
-      mapAsync(async i => {
-        await abortableSleep(100);
-        return i * i;
-      }),
-      filterAsync(async i => i % 2 == 0)
-    ).valueAsync(findAsync())
+  const [output, time] = await performanceAsync(
+    async () =>
+      await fromAsAsync([1, 2, 3, 4, 5])
+        .pipe(
+          mapAsync(async i => {
+            await abortableSleep(100);
+            return i * i;
+          }),
+          filterAsync(async i => i % 2 == 0)
+        )
+        .valueAsync(findAsync())
   );
 
   expect(output).toEqual(4);
@@ -39,20 +44,23 @@ test('async operator: simple aborted 1', async () => {
   controller.abort();
   const signal = controller.signal;
 
-  const [output,time] = await performanceAsync(async () =>
-
-    await from([1, 2, 3, 4, 5]).pipe(reverse()).to(async()).pipe(
-      tapAsync(async () => await abortableSleep(20, signal)),
-      mapAsync(async i => {
-        await abortableSleep(20, signal);
-        return i * i;
-      })
-    ).toArrayAsync()
-
+  const [output, time] = await performanceAsync(
+    async () =>
+      await from([1, 2, 3, 4, 5])
+        .pipe(reverse())
+        .to(async())
+        .pipe(
+          tapAsync(async () => await abortableSleep(20, signal)),
+          mapAsync(async i => {
+            await abortableSleep(20, signal);
+            return i * i;
+          })
+        )
+        .toArrayAsync()
   );
 
-  expect(output).toEqual([25,16,9,4,1]);
-  expect(time < 20 ).toBe(true);
+  expect(output).toEqual([25, 16, 9, 4, 1]);
+  expect(time < 20).toBe(true);
 });
 
 test('async operator: simple aborted 2', async () => {
@@ -61,18 +69,21 @@ test('async operator: simple aborted 2', async () => {
 
   setTimeout(() => controller.abort(), 100);
 
-  const [output,time] = await performanceAsync(async () =>
-
-    await from([1, 2, 3, 4, 5]).pipe(reverse()).to(async()).pipe(
-      tapAsync(async i => await abortableSleep(20, signal)),
-      mapAsync(async i => {
-        await abortableSleep(20, signal);
-        return i * i;
-      })
-    ).toArrayAsync()
-
+  const [output, time] = await performanceAsync(
+    async () =>
+      await from([1, 2, 3, 4, 5])
+        .pipe(reverse())
+        .to(async())
+        .pipe(
+          tapAsync(async i => await abortableSleep(20, signal)),
+          mapAsync(async i => {
+            await abortableSleep(20, signal);
+            return i * i;
+          })
+        )
+        .toArrayAsync()
   );
-  
-  expect(output).toEqual([25,16,9,4,1]);
-  expect(time > 100 && time < 200 ).toBe(true);
+
+  expect(output).toEqual([25, 16, 9, 4, 1]);
+  expect(time > 100 && time < 200).toBe(true);
 });
--- packages/leseq/tests/testUtil.ts
+++ packages/leseq/tests/testUtil.ts
@@ -1,22 +1,20 @@
-export const abortableSleep = (ms: number, signal?: AbortSignal) => new Promise<void>(resolve => {
-  if(signal != null) {
-    if(signal.aborted) resolve();
-    const listener = () => {
-      signal.removeEventListener('abort',listener);
-      resolve();
+export const abortableSleep = (ms: number, signal?: AbortSignal) =>
+  new Promise<void>(resolve => {
+    if (signal != null) {
+      if (signal.aborted) resolve();
+      const listener = () => {
+        signal.removeEventListener('abort', listener);
+        resolve();
+      };
+      signal.addEventListener('abort', listener);
     }
-    signal.addEventListener('abort',listener);
-  }
-  setTimeout(resolve,ms)
-});
+    setTimeout(resolve, ms);
+  });
 
 export const performanceAsync = async <T>(func: () => Promise<T>) => {
   const startTime = performance.now();
-  const result = await func(); 
+  const result = await func();
   const endTime = performance.now();
 
-  return [
-    result,
-    endTime - startTime,
-  ]
-}
+  return [result, endTime - startTime] as const;
+};
